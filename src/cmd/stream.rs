2eb1d7afd4470ddb573c12ade933888fee44f55f
Jeremy Cooper <jeremy.gthb@baymoo.org>
2026-03-17 12:56:21 -0700

    Add help metadata to stream command.

--- src/cmd/stream.rs
+++ src/cmd/stream.rs
@@ -1,10 +1,17 @@
 use crate::size::SizeSpec;
 use clap::Parser;
 
+/// Stream data to a Glacier vault.
 #[derive(Debug, Parser)]
 pub struct Cmd {
+    /// AWS region in which the destination vault resides. Example: us-west-1
     region: String,
+    /// Destination vault identifier. NOT FULL ARN, only last part. 
+    /// Example: photos-audio
     vault: String,
+    /// A description of the archive being uploaded.
     description: String,
+    /// Estimated size of upload. Human readable sizes are supported.
+    /// Units: b, k, m, g, and t. Example: 1.1t
     size: SizeSpec,
 }
