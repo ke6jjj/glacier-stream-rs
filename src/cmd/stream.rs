2eda127532c2d97c2134ccc6e5e49212a1b1fd1c
Jeremy Cooper <jeremy.gthb@baymoo.org>
2026-03-17 16:37:48 -0700

    Refactor for some clarity.
    Refactor, just for clarity, so that higher level operations can be
    understood in the main command handler.
    
    I will soon attempt an experiment wherein I spawn several concurrent
    uploaders to see if we can improve the overall upload speed.

--- src/cmd/stream.rs
+++ src/cmd/stream.rs
@@ -4,6 +4,7 @@ use std::io::{self, Read};
 use aws_config::{BehaviorVersion, Region};
 use aws_sdk_glacier::client::Client as GlacierClient;
 use aws_sdk_glacier::primitives::ByteStream;
+use aws_sdk_glacier::operation::initiate_multipart_upload::InitiateMultipartUploadOutput;
 
 /// Stream data to a Glacier vault.
 #[derive(Debug, clap::Args)]
@@ -22,24 +23,41 @@ pub struct Cmd {
 
 impl Cmd {
     pub async fn run(&self) -> Result {
+        let client = self.get_client().await?;
         let part_size = crate::util::part_size_for_size(self.size.to_bytes());
         eprintln!("Using part size: {} bytes", part_size);
+        let upload = self.initiate_upload(&client, part_size).await?;
+        let upload_id = upload
+            .upload_id()
+            .ok_or_else(|| Error::msg("Failed to initiate multipart upload: missing upload ID"))?;
+        eprintln!("Upload initiated. Upload ID: {}", upload_id);
+        self.upload(part_size, &client, upload_id).await?;
+        self.complete_upload(&client, upload_id).await?;
+        eprintln!("Upload complete.");
+        Ok(())
+    }
+
+    async fn get_client(&self) -> Result<GlacierClient> {
         let region = Region::new(self.region.clone());
         let config = aws_config::defaults(BehaviorVersion::latest())
             .region(region)
             .load()
             .await;
         let client = GlacierClient::new(&config);
+        Ok(client)
+    }
+
+    async fn initiate_upload(&self, client: &GlacierClient, part_size: u64) -> Result<InitiateMultipartUploadOutput> {
         let upload = client.initiate_multipart_upload()
             .vault_name(&self.vault)
+            .part_size(part_size.to_string())
             .archive_description(&self.description)
-            .part_size(format!("{}", part_size as i64))
             .send()
             .await?;
-        let upload_id = upload
-            .upload_id()
-            .ok_or_else(|| Error::msg("Failed to initiate multipart upload: missing upload ID"))?;
-        eprintln!("Upload initiated. Upload ID: {}", upload_id);
+        Ok(upload)
+    }
+
+    async fn upload(&self, part_size: u64, client: &GlacierClient, upload_id: &str) -> Result {
         let mut buffer = Vec::new();
         for part_number in 1.. {
             let mut chunk_reader = io::stdin().take(part_size);
@@ -59,12 +77,15 @@ impl Cmd {
                 .send()
                 .await?;
         }
+        Ok(())
+    }
+
+    async fn complete_upload(&self, client: &GlacierClient, upload_id: &str) -> Result {
         client.complete_multipart_upload()
             .vault_name(&self.vault)
             .upload_id(upload_id)
             .send()
             .await?;
-        eprintln!("Upload complete.");
         Ok(())
     }
 }
