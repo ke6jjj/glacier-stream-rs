ae0aa15f19034413f12710a0f1558c1690960f0b
Jeremy Cooper <jeremy.gthb@baymoo.org>
2026-03-26 19:45:40 -0700

    Update tree-hash for parallelism.

--- src/cmd/tree_hash.rs
+++ src/cmd/tree_hash.rs
@@ -11,8 +11,16 @@ use std::io::{self, Read};
 /// large files. This command computes the tree hash of a local stream
 /// (on stdin) so that it can be compared with the tree hash of an uploaded
 /// archive, ensuring that the upload is correct.
+///
+/// Since tree hashes are computed in parts, the full computation can be
+/// parallelized. This command allows you to specify the number of concurrent
+/// hash workers to speed up the computation, especially in situationes where
+/// the input data is large and the system has multiple CPU cores available.
 #[derive(Debug, clap::Parser)]
 pub struct Cmd {
+    /// Number of concurrent hash workers. Default: 4
+    #[arg(short, long, default_value_t = 4)]
+    workers: usize,
     #[arg(short, long, default_value_t = false)]
     /// Be verbose. Show total bytes read as well.
     verbose: bool,
@@ -20,28 +28,106 @@ pub struct Cmd {
 
 impl Cmd {
     pub async fn run(&self) -> EasyResult<()> {
-        let mut tree = SequentialTreeHash::new();
-        let mut buffer = Vec::with_capacity(AWS_TREE_HASH_PART_SIZE as usize); // 1 MiB buffer
+        let work_queue = WorkQueue::from(tokio_mpmc::channel(self.workers));
+        let result_queue = ResultQueue::from(tokio_mpmc::channel(self.workers));
+        let collector_task = tokio::spawn(hash_collector(result_queue.rx.clone()));
+        // Spawn hash workers
+        let mut worker_tasks = tokio::task::JoinSet::new();
+        for _ in 0..self.workers {
+            worker_tasks.spawn(
+                hash_worker(work_queue.rx.clone(), result_queue.tx.clone())
+            );
+        }
         let mut total_read: u64 = 0;
         loop {
-            buffer.clear(); // Reuse buffer; otherwise read_to_end appends and memory grows without bound.
+            let mut buffer = Vec::with_capacity(AWS_TREE_HASH_PART_SIZE as usize); // 1 MiB buffer
             let mut chunk_reader = io::stdin().take(AWS_TREE_HASH_PART_SIZE);
             let bytes_read = chunk_reader.read_to_end(&mut buffer)?;
-            if bytes_read == 0 && total_read > 0 {
-                break;
-            }
-            total_read += bytes_read as u64;
-            let hash = Sha256::digest(&buffer);
-            tree.insert(hash.into());
             if bytes_read == 0 {
                 break;
             }
+            work_queue.tx.send(HashWorkerCommand {
+                offset: total_read,
+                data: buffer,
+            }).await?;
+            total_read += bytes_read as u64;
         }
-        let tree_hash = tree.finalize()?;
         if self.verbose {
             println!("Total bytes read: {}", total_read);
         }
+        for worker in worker_tasks.join_next().await {
+            worker??;
+        }
+        let tree_hash = collector_task.await??;
         println!("Tree hash: {}", hex::encode(tree_hash));
         Ok(())
     }
+}
+
+struct HashWorkerCommand {
+    offset: u64,
+    data: Vec<u8>,
+}
+
+type WorkRxChannel = tokio_mpmc::Receiver<HashWorkerCommand>;
+type WorkTxChannel = tokio_mpmc::Sender<HashWorkerCommand>;
+
+struct WorkQueue {
+    tx: WorkTxChannel,
+    rx: WorkRxChannel,
+}
+
+impl From<(WorkTxChannel, WorkRxChannel)> for WorkQueue {
+    fn from((tx, rx): (WorkTxChannel, WorkRxChannel)) -> Self {
+        Self { tx, rx }
+    }
+}
+
+struct HashResult {
+    offset: u64,
+    hash: [u8; 32],
+}
+
+type ResultRxChannel = tokio_mpmc::Receiver<HashResult>;
+type ResultTxChannel = tokio_mpmc::Sender<HashResult>;
+
+struct ResultQueue {
+    tx: ResultTxChannel,
+    rx: ResultRxChannel,
+}
+
+impl From<(ResultTxChannel, ResultRxChannel)> for ResultQueue {
+    fn from((tx, rx): (ResultTxChannel, ResultRxChannel)) -> Self {
+        Self { tx, rx }
+    }
+}
+
+async fn hash_worker(work_chan: WorkRxChannel, result_chan: ResultTxChannel) -> EasyResult<()> {
+    while let Some(command) = work_chan.recv().await? {
+        let mut hasher = Sha256::new();
+        hasher.update(&command.data);
+        let hash_result = HashResult {
+            offset: command.offset,
+            hash: hasher.finalize().into(),
+        };
+        result_chan.send(hash_result).await?;
+    }
+    Ok(())
+}
+
+async fn hash_collector(result_chan: ResultRxChannel) -> EasyResult<[u8; 32]> {
+    let mut tree_hasher = SequentialTreeHash::new();
+    let mut total_hashed: u64 = 0;
+    // Chunks may arrive out of order, so we store them in a map until we can
+    // write them out and hash them in the correct order.
+    let mut chunk_map = std::collections::BTreeMap::new();
+    while let Some(part) = result_chan.recv().await? {
+        chunk_map.insert(part.offset, part);
+        // Write out any contiguous chunks starting from total_hashed
+        while let Some(part) = chunk_map.remove(&total_hashed) {
+            tree_hasher.insert(part.hash);
+            total_hashed += AWS_TREE_HASH_PART_SIZE;
+        }
+    }
+    Ok(tree_hasher.finalize()?)
 }
